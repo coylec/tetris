c15ac68bd416ebfd461606c86d61d8553a49044b
Conrad Coyle <coylec@users.noreply.github.com>
2020-07-17 13:58:23 +0100

    add tetrominos

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ document.addEventListener('DOMContentLoaded', () => {
         document.getElementById("main").appendChild(div);
     }
 
+    let color = { background: "#5c2a3b",     // background
+                        wall: "#d83c66",     // walls
+                        solid: "#49b5ab",     // solid tetromino
+                        tetromino: "#e97539" };   // falling tetromino
+
 
     const grid = document.querySelector('.grid');
     let squares = Array.from(document.querySelectorAll('.grid div'));
@@ -11,6 +16,35 @@ document.addEventListener('DOMContentLoaded', () => {
     const StartBtn = document.getElementById('start-button');
     const width = 10;
     
+    // Tetrominos
+
+    const jShape = [0,0,1, 0,0,1, 0,1,1];
+
+    const lShape = [1,0,0, 1,0,0, 1,1,0];
+
+    const tShape = [0,0,0, 0,1,0, 1,1,1];
+
+    const sShape = [0,0,0, 0,1,1, 0,1,1];
+
+    const zShape = [0,0,0, 1,1,0, 0,1,1];
+
+    const squareShape = [1,1,0, 1,1,0, 0,0,0];
+
+    const iShape = [0,0,0, 1,1,1, 0,0,0];
+
+    const tetrominos = [jShape, lShape, tShape, sShape, zShape, squareShape, iShape];
+
+    let current = [0,0,0, 0,0,0, 0,0,0];
+    let next = [0,0,0, 0,0,0, 0,0,0];
+
+    function makeRandom() {
+        let index = Math.floor((Math.random() * tetrominos.length));
+        return [...tetrominos[index]];
+    }
+
+    current = makeRandom();
+    next = makeRandom();
+
     
 
 
